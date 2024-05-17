fda33f0ff5f76f131ef95d6a07f0cb746a72ab3c
damianGG <damiangolon@gmail.com>
2024-05-17 11:37:22 +0200

    feat: Update page layout and styles for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-"use client"
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import NavbarOne from "@/components/blocks/navbar/navbar-1/NavbarOne";
 import Hero4 from "@/components/blocks/hero/Hero4";
 import About6 from "@/components/blocks/about/About6";
-import Hero2 from "@/components/blocks/hero/Hero2";
 
 export default function Home() {
   return (
     <main >
-      <div className="">
+      <div>
         <Hero4 />
         <About6 />
       </div>
