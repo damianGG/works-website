63259c0cb8421a7bc7332d48609f169ce99841ce
damianGG <damiangolon@gmail.com>
2024-08-22 16:41:57 +0200

    chore: Update npm dependencies and add i18n configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,17 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'aktywnekobiety.pl',
+                hostname: 'integracja-adaptacja.pl',
             },
             {
                 protocol: 'https',
                 hostname: 'jpmcg.up.railway.app',
             },
         ],
+    },
+    i18n: {
+        locales: ['pl', 'u'], // Lista obsługiwanych języków
+        defaultLocale: 'pl',   // Domyślny język
     },
 };
 
